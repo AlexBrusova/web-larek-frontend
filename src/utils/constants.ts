cc94c3f8c7d07484a23a01b4884225689a7be0dd
Alexandra Brusova <mary15479@gmail.com>
2024-09-26 00:00:41 +0300

    Updated readme.md documentation

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -7,9 +7,9 @@ export const settings = {
 };
 
 export const categoryMap: CategoryMap = {
-  другое: 'card__category_other',
+  'другое': 'card__category_other',
   'софт-скил': 'card__category_soft',
-  дополнительное: 'card__category_additional',
-  кнопка: 'card__category_button',
+  'дополнительное': 'card__category_additional',
+  'кнопка': 'card__category_button',
   'хард-скил': 'card__category_hard',
 };
